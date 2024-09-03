1d915cb5b1fde0687369388b8baa56edf6affc27
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-09-03 19:12:42 +0200

    transposition table

--- index.js
+++ index.js
@@ -7,11 +7,32 @@ class Result{
   }
 }
 
+function get_hash(oxo){
+  return oxo.getStringBoard();
+}
+
+function save_result(oxo, result){
+  const hash = get_hash(oxo);
+  transposition_table[hash] = result;
+}
+
+function get_result(oxo){
+  const hash = get_hash(oxo);
+  return transposition_table[hash];
+}
+
 function negamax(oxo, deep=1){
   // if current player just lost
   if (oxo.isWinning())return new Result(-(9-deep), null);
   // draw
   if (oxo.isFinished())return new Result(0, null);
+  // transposition_table
+  let transposition_table_result = get_result(oxo);
+  if (transposition_table_result){
+    // count the number of times we used the transposition table
+    transposition_table_used_times++;
+    return transposition_table_result;
+  }
 
   let best_move = -1;
   let best_score = -Infinity;
@@ -19,18 +40,23 @@ function negamax(oxo, deep=1){
     oxo.makeMove(move);
 
     let result = negamax(oxo, deep+1);
-    result.score = -result.score;
+    let score = -result.score
 
-    if (result.score > best_score){
-      best_score = result.score;
+    if (score > best_score){
+      best_score = score;
       best_move = move;
     }
     
     oxo.undoLastMove();
   }
 
-  return new Result(best_score, best_move);
+  let result = new Result(best_score, best_move);
+  save_result(oxo, result);
+  return result;
 }
 
 let oxo = new Oxo();
+let transposition_table = {};
+let transposition_table_used_times = 0;
 console.log(negamax(oxo));
+console.log(`transposition table used ${transposition_table_used_times} times`);
