8a0b95992f5a29a63ba488aebd3d1e785d802d5e
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-09-03 18:53:26 +0200

    negamax

--- index.js
+++ index.js
@@ -7,32 +7,8 @@ class Result{
   }
 }
 
-function mini(oxo, deep=1){
-  // if minimizer just lost
-  if (oxo.isWinning())return new Result(9-deep, null);
-  // draw
-  if (oxo.isFinished())return new Result(0, null);
-
-  let best_move = -1;
-  let best_score = Infinity;
-  for (const move of oxo.getMoves()){
-    oxo.makeMove(move);
-
-    let result = maxi(oxo, deep+1);
-
-    if (result.score < best_score){
-      best_score = result.score;
-      best_move = move;
-    }
-    
-    oxo.undoLastMove();
-  }
-
-  return new Result(best_score, best_move);
-}
-
-function maxi(oxo, deep=1){
-  // if maximizer just lost
+function negamax(oxo, deep=1){
+  // if current player just lost
   if (oxo.isWinning())return new Result(-(9-deep), null);
   // draw
   if (oxo.isFinished())return new Result(0, null);
@@ -42,7 +18,8 @@ function maxi(oxo, deep=1){
   for (const move of oxo.getMoves()){
     oxo.makeMove(move);
 
-    let result = mini(oxo, deep+1);
+    let result = negamax(oxo, deep+1);
+    result.score = -result.score;
 
     if (result.score > best_score){
       best_score = result.score;
@@ -56,4 +33,4 @@ function maxi(oxo, deep=1){
 }
 
 let oxo = new Oxo();
-console.log(maxi(oxo));
+console.log(negamax(oxo));
